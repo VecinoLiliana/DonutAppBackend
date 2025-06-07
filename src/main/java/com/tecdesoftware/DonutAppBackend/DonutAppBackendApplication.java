0e915ce37ed4f7f2bbada63e845142a1db97e5e1
lili <vecinocollililianamarisol@gmail.com>
2025-06-06 18:14:27 -0600

    first commit

---
+++ src/main/java/com/tecdesoftware/DonutAppBackend/DonutAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.DonutAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonutAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonutAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tecdesoftware/DonutAppBackend/DonutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.DonutAppBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
